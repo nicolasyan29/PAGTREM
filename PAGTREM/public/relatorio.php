34a359d2769a8fed73fdfea9081e4afb6d55bdec
nicolasyan29 <nicolas_borba@estudante.sesisenai.org.br>
2025-11-11 12:07:07 -0300

    add

--- PAGTREM/public/relatorio.php
+++ PAGTREM/public/relatorio.php
@@ -14,7 +14,7 @@ $cards = [
     <meta charset="UTF-8">
     <title><?php echo $titulo; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../style/relatio.css">
+    <link rel="stylesheet" href="../style/relatorio.css">
 </head>
 <body>
     <div class="app">
