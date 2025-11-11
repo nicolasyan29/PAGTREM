9efdf9853cf20b9b9093e9878a81f49c84ee011e
nicolasyan29 <nicolas_borba@estudante.sesisenai.org.br>
2025-11-11 11:46:23 -0300

    add

--- PAGTREM/public/dashboard.php
+++ PAGTREM/public/dashboard.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style/dashboard.css">
 </head>
 <body>
     <div class="dashboard">
