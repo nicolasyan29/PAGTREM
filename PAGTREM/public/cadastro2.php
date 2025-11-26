ff42016b0c68aadd9a670f7dd9a523ff5fcd312d
nicolasyan29 <nicolas_borba@estudante.sesisenai.org.br>
2025-11-26 09:55:30 -0300

    add

--- PAGTREM/public/cadastro2.php
+++ PAGTREM/public/cadastro2.php
@@ -4,23 +4,16 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $local = trim($_POST["local"]);
-    $foto = $_FILES["foto"];
 
     if (strlen($local) < 2) {
         $erro = "Localização inválida!";
-    }
-    elseif ($foto["error"] !== 0) {
-        $erro = "Envie uma foto válida!";
     }
     else {
         
         $_SESSION["local"] = $local;
 
         
-        $nomeFoto = "foto_" . time() . ".jpg";
-        move_uploaded_file($foto["tmp_name"], "uploads/" . $nomeFoto);
-
-        $_SESSION["foto"] = $nomeFoto;
+        $_SESSION["foto"] = null;
 
         header("Location: menu.php");
         exit;
@@ -30,15 +23,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head><meta charset="UTF-8"><title>Cadastro - Parte 2</title><link rel="stylesheet" href="../style/combined.css">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro - Parte 2</title>
+    <link rel="stylesheet" href="../style/combined.css">
 </head>
 <body>
-    <form method="POST" enctype="multipart/form-data">
+
+    <form method="POST">
         <input type="text" name="local" placeholder="Localização" required><br>
-        <input type="file" name="foto" accept="image/*" required><br>
         <button type="submit">Finalizar Cadastro</button>
     </form>
 
     <?php if(isset($erro)) echo "<p style='color:red;'>$erro</p>"; ?>
+
 </body>
 </html>
