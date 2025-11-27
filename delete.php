d6d27b77a62eb6031069b554e27c2902307d6219
nicolasyan29 <nicolas_borba@estudante.sesisenai.org.br>
2025-11-27 08:57:37 -0300

    add crud

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'db.php';
+$id = $_GET['id'];
+
+$sql = " DELETE FROM usuarios WHERE id=$id ";
+
+if ($conn->query($sql) === true) {
+    echo "Registro excluído com sucesso.
+        <a href='read.php'>Ver registros.</a>
+        ";
+} else {
+    echo "Erro " . $sql . '<br>' . $conn->error;
+}
+$conn -> close();
+exit();
---
+++ read.php
@@ -0,0 +1,43 @@
+<?php
+
+include 'db.php';
+
+$sql = "SELECT * FROM usuarios";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    echo "<table border ='1'>
+        <tr>
+            <th> ID </th>
+            <th> Nome </th>
+            <th> Email </th>
+            <th> Data de Criação </th>
+            <th> Ações </th>
+        </tr>
+         ";
+
+    while ($row = $result->fetch_assoc()) {
+
+        echo "<tr>
+                <td> {$row['id']} </td>
+                <td> {$row['name']} </td>
+                <td> {$row['email']} </td>
+                <td> {$row['created_at']} </td>
+                <td> 
+                    <a href='update.php?id={$row['id']}'>Editar<a>
+                    <a href='delete.php?id={$row['id']}'>Excluir<a>
+                
+                </td>
+              </tr>   
+        ";
+    }
+    echo "</table>";
+} else {
+    echo "Nenhum registro encontrado.";
+}
+
+$conn -> close();
+
+echo "<a href='create.php'>Inserir novo Registro</a>";
