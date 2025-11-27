5471b0d470a3b723c3fb27d51b4140ffdb65f299
MathKing1 <matheus_n_cardoso@estudante.sesisenai.org.br>
2025-11-27 10:08:49 -0300

    addupd

--- PAGTREM/public/tela_cadastro_2.php
+++ PAGTREM/public/tela_cadastro_2.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Cadastro - Parte 2</title>
-    <link rel="stylesheet" href="../style/combined.css">
+    <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
 
--- PAGTREM/public/tela_chat.php
+++ PAGTREM/public/tela_chat.php
@@ -33,7 +33,7 @@ $conn->close();
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Chat Suporte</title>
-<link rel="stylesheet" href="../style/combined.css">
+<link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
 <div class="phone">
