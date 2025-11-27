566cce1b29dbf19f87de27491e7eb525b85194dd
MathKing1 <matheus_n_cardoso@estudante.sesisenai.org.br>
2025-11-27 07:49:00 -0300

    add upd menu

--- PAGTREM/public/menu.php
+++ PAGTREM/public/menu.php
@@ -18,13 +18,22 @@ if (!isset($_SESSION["logado"])) {
     <link rel="stylesheet" href="../style/combined.css">
 </head>
 <body>
-    
 
 <div class="container">
     <h2>Menu Principal</h2>
 
-    <button onclick="window.location.href='dashboard.php'">Ir para Dashboard</button>
+    <button onclick="window.location.href='dashboard.php'">📊 Ir para Dashboard</button>
+    <br><br>
+
+    <button onclick="window.location.href='gestaoderotas.php'">🛤️ Gestão de Rotas</button>
+    <br><br>
+
+    <button onclick="window.location.href='relatorio.php'">📑 Relatórios</button>
+    <br><br>
+
+    <button onclick="window.location.href='notificacoes.php'">🔔 Notificações</button>
 </div>
+
 </body>
 
 </html>
